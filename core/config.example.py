7ab3fb02ea8462852e6a6beab54794ed2a6caaa7
Mr Stonkles <voby@waifu.club>
2026-07-29 19:57:17 +0600

    Untrack core/config.py, ignore config.py, and add config.example.py

--- core/config.example.py
+++ core/config.example.py
@@ -1,9 +1,7 @@
-import os
-
-TOKEN = os.getenv("TOKEN", "YOUR_BOT_TOKEN_HERE")
+TOKEN = "YOUR_DISCORD_BOT_TOKEN_HERE"
 DEBUG_MODE = 0
 RAID_MESSAGE = """
-# > FUCKED BY [VOID SENTINEL](https://discord.gg/Zwa2Jx6vf7)
+# > FUCKED BY [VOID SENTINEL](https://discord.gg/Zwa2Jx6vf7) 
 # YALL NEED BETTER SECURITY @everyone
 \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n
 \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n
